2a60fe3292e3c470c8e7c944c2d491921afb91d7
Zack Spann <zspann@zspann-macbookpro3.roam.corp.google.com>
2017-04-17 10:35:05 -0700

    one funtion seems non-standard.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
-//define functions here
+//1
+function getIt() {
+  alert("Hey!");
+}
+//2
+function frameIt() {
+  $(this).addclass(tasty);
+  $(this).css('border', '3m soid red');
+}
+//3 I wrote this one to accept a parameter (each keystroke) - i'm not sure
+// if that will function correctly but it seems simpler.
+function pressIt(n) {
+  if(n.which == 71) {
+    alert("You have pressed the G key");
+  }
+}
+//4
+function submitIt() {
+  alert("your form is going to submit now");
+  return;
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+//1
+  $('p').click(getIt())
+//2
+  $('img').load(frameIt())
+//3
+  $('form').keydown(pressIt(n))
+//4
+  $('form').submit(submitIt())
 });
